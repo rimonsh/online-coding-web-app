fceb473b7f61314fe2f897ab9d79844e45955101
rimonsh <rimonsh@mta.ac.il>
2023-12-05 01:23:43 +0200

    server port change

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
