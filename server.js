70938d8b917a22d92547a36834695d1b5a28fbb4
rimonsh <rimonsh@mta.ac.il>
2023-12-04 19:53:18 +0200

    configuring first connection to be on mentor read-only mode

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
-const http = require("http");
 const express = require("express");
+const http = require("http");
 const socketIO = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+  transports: ["websocket"],
+});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+let mentorConnected = false;
+let mentorSocket = null;
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  if (!mentorConnected) {
+    mentorConnected = true;
+    mentorSocket = socket;
+    socket.emit("mentor-connected");
+    console.log("A Mentor connected");
+  } else {
+    socket.emit("student-connected");
+    console.log("A Student connected");
+  }
 
   socket.on("code-change", (newCode) => {
     socket.broadcast.emit("code-change", newCode);
   });
 
   socket.on("disconnect", () => {
+    if (socket === mentorSocket) {
+      console.log("A Mentor disconnected");
+      mentorConnected = false;
+      mentorSocket = null;
+    }
     console.log("User disconnected");
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/codeBlockPage.js
+++ src/codeBlockPage.js
@@ -5,25 +5,39 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 import codeBlocks from "./codeBlocks";
 
-const socket = io("http://localhost:3000"); // Replace with your server URL
+const socket = io("http://localhost:5000", {
+  transports: ["websocket"],
+});
 
 const CodeBlock = () => {
   const { id } = useParams();
   const codeBlock = codeBlocks[id];
   const [code, setCode] = useState(codeBlock.code);
+  const [isMentor, setIsMentor] = useState(false);
 
   useEffect(() => {
+    socket.on("mentor-connected", () => {
+      setIsMentor(true);
+    });
+
+    socket.on("student-connected", () => {
+      setIsMentor(false);
+    });
+
     socket.on("code-change", (newCode) => {
       setCode(newCode);
     });
 
     return () => {
+      socket.off("student-connected");
       socket.off("code-change");
     };
   }, []);
 
   const handleCodeChange = (newCode) => {
-    setCode(newCode); // Update the code immediately for the current user
+    if (!isMentor) {
+      setCode(newCode); // Update the code immediately for the current user
+    }
     socket.emit("code-change", newCode);
   };
 
@@ -33,7 +47,7 @@ const CodeBlock = () => {
       <SyntaxHighlighter language="javascript" style={tomorrow}>
         {code}
       </SyntaxHighlighter>
-      {codeBlock.readOnly ? (
+      {isMentor ? (
         <p>Read-only mode (Mentor)</p>
       ) : (
         <textarea
