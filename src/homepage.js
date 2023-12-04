39cb3b3fb5c12e04d10bf9441acbefbcdd1ecd5a
rimonsh <rimonsh@mta.ac.il>
2023-12-04 14:52:10 +0200

    adding a homepage component

---
+++ src/homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1> Hello! Welcome To The Online Coding Web Application!</h1>
+      <h2> Choose code block</h2>
+    </div>
+  );
+};
+
+export default Homepage;
